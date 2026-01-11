39fb9646aa0d330e1dacd7b211ad0f9994eee5bc
ANM <anmtechept7@gmail.com>
2026-01-11 15:50:36 +0530

    React Router for learning

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-gray-800 text-4xl bg-blue-100 rounded-3xl p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
